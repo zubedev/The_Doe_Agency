62a9dcf1aa47053a345e524e3ae038ee08447f4a
Md Zubair Beg <ziibii88@gmail.com>
2021-07-19 00:18:20 +0800

    added wait-for-db mgmt cmd; enabled workflow migration

---
+++ core/management/commands/wait_for_db.py
@@ -0,0 +1,18 @@
+import time
+
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_conn = None
+        while not db_conn:
+            try:
+                db_conn = connections["default"]
+            except OperationalError:
+                self.stdout.write("Database not available, waiting 1 second..")
+                time.sleep(1)
+        self.stdout.write(self.style.SUCCESS("Database available"))
